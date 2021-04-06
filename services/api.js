b00a94f7240c26d7b18e1f7d19c833d42b65a439
Kaio Geglio <kaiogeglio@gmail.com>
2021-04-06 18:31:00 -0300

    0.1

--- services/api.js
+++ services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  // baseURL: "https://admin-portal-over.herokuapp.com"
-  baseURL: "http://localhost:8080"
+  baseURL: "https://admin-portal-over.herokuapp.com"
+  // baseURL: "http://localhost:8080"
 })
 
 api.interceptors.request.use(async config => {
